054830ba05ad8b7274693ac3b31057b247c1aba8
SmugTheKiler <68525883+SmugTheKiler@users.noreply.github.com>
2021-07-28 14:18:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-// this is the main file.
+// make sure to read the last part before this one!
+
 const Discord = require('discord.js')
-//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
-/* require Discord to use it every
-where in code, and access its API.
-*/
-      
- const client = new Discord.Client;
-//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-/* Create your bot it self, and
-log in with it on the buttom of the
-code! */
-  
+
+const client = new Discord.Client;
+
+const config = require('./config.json')
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+/* Find the file among your files! */
+
+// Now, you can simply do stuff like this! :
+const token = config.token;
+//^^^^^^^^^^^^^^^^^^^^^^^^^
+/* you can do config.( something
+that you have setted up in config.json)
+to have it!*/
+
 client.on("ready", () => {
 log(`Hey! I am ready to go! \n Me online.`)
-})
+}) 
 
 
-client.login('Token from discord developer portal')
-
-// and there we go, you now have your bot signed in and running on discord.
-
-// This bot currently has no functions.
+client.login(token)
+//^^^^^^^^^^^^^^^^^
+/* You just got the
+token from config.json!
+*/
