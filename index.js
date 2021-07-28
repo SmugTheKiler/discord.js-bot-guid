b832ba67aab167f2ba0e2f58fb7513ca6811b60c
SmugTheKiler <68525883+SmugTheKiler@users.noreply.github.com>
2021-07-28 20:30:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,4 +27,20 @@ log(`just recived a message! The message had this in it: \n ${message}`)
 // now it ends the event
 });
 
+/*
+So heres more details. Baiscly client is the bot it self, and on is like when you are on a hill, and see something happen
+somewhere down there, so if something happens, it checks if there is an event for that. If there is none, it will simply
+return completly nothing. If there is, it will attempt to run code in it, and if there are intervals, it will run them.
+
+so here it is:
+
+client.on('event name', (get some stuff from previus code, because if you dont, it will have nothing you made your self like client, message, ect.) => {
+//some code for this event                                                                                                                        ^^^^^^
+//                                                                                                                                             This basicly
+                                                                                                                                               runs the code
+                                                                                                                                               that is in the
+                                                                                                                                               {}.
+}
+*/
+
 client.login(token)
