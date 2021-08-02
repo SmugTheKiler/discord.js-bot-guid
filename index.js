e2b5c5fc99d491a516305a60be0d89679c88ae67
SmugTheKiler <68525883+SmugTheKiler@users.noreply.github.com>
2021-08-02 22:32:19 +0200

    Added all nececery changes.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // make sure to read the last part before this one!
 
-// Lets make the very first command!
+// Here we will start making commands to moderate our bots!
 const Discord = require('discord.js')
 
 const client = new Discord.Client;
@@ -11,23 +11,8 @@ const token = config.token;
 
 const log = console.log;
 
-/* 
-Before we add some commands, lets instead make sure
-our bot has a prefix. We do not want the bot to simply
-respond when a use sends "help" 
-It would confuse the user since he probably asked someone
-else for help, and not a bot. This way users can choose
-if they are talking to a bot, and the bot can deside if 
-someone is trying to do something with it.
-*/
-
 const prefix = config.prefix;
-/*^^^^^^^^^^^^^^^^^^^^^^^^^^
-you will now have to add this:
-{
-  "prefix" : "Your awesome prefix"
-}
-*/
+
 client.on("ready", () => {
 log(`Hey! I am ready to go! \n Me online.`)
 }) 
@@ -40,40 +25,41 @@ log(`just recived a message! The message had this in it: \n ${message}`)
   
   
   	if (!message.content.startsWith(prefix) || message.author.bot) return;
-  /*^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  check the message starts with your prefix, which we define in your
-  config.json file just like the token. Then, if it does, or it doesnt
-  but the message it self is sent by a bot, it will just simply return 
-  complitly.... nothing. */
-  
+
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
-/*^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  devide the words after the prefix.*/
-	const command = args.shift().toLowerCase(); // Make a short cut to not write this every time. This basicly checks for a new command.
+
+	const command = args.shift().toLowerCase();
   
 	if (command === 'ping') {
-  /*^^^^^^^^^^^^^^^^^^^^^^^
-  the if statment that checks
-  what the command it self is.
-  If the command is this, then
-  it would run the code under this.*/
+
 		message.channel.send('Pong.');
-    //replies with "Pong." in the channel
     
 	} else if (command === 'beep') {
- //*^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
- //  a brang new command with the new name.
-		message.channel.send('Boop.');
-	}
-  // this can just go on and on, all you have to is a simply change the name, and the code.
-  /* } (<= assuming you already had a command and your adding this to it) else if (command === 'Command name here') { //if the command is ran with this name, the following code will be ranned:
-  
-  // code here :)
-  
-  }
-});
-*/
-client.login(token)
 
-  
-  //The command handler above is from discordjs.guide. We recommend you taking a look into it. 
+		message.channel.send('Boop.');
+	} else if (command === 'kick') { // lets make a new command!
+		
+	if (!message.mentions.users.size) {
+// 	check if a user has pinged someone
+//      he or she wanted to kick from the
+//      server.
+	return message.reply('you need to tag a user in order to kick them!');
+//      if he or she did not, it would send this sending that he or she did not
+//      ping anyone.
+	}
+
+	const torget = message.mentions.users.first();
+//      define the what where going to call "torget", which is what were going to attempt to kick.
+	if(!torget.kickable) {
+//      check if the torget is kickable 
+		message.channel.send('I am unable to kick this user...')
+//              if not then just send this to let the user know he or she cant be kicked
+	}
+//      if the torget passes all this, it would try to kick the user.
+	torget.kick();
+//      kick the user.
+	message.channel.send(`I kicked ${torget} succesfully!`)
+//      let the user know he or she was kicked.
+	}
+
+client.login(token)
